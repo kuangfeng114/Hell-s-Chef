7aef45b4dab2bed4f3f1c798ea0ac1e05ecc298a
kuangfeng114 <1026744516@qq.com>
2026-01-07 00:56:25 +0800

    0.3.2
    1.添加了程序化切菜
    2.再次修复飞天bug
    下一步：厨具拿取和炒菜实现

--- Hell's chef/Assets/script/CookingPlate.cs
+++ Hell's chef/Assets/script/CookingPlate.cs
@@ -5,17 +5,34 @@ using UnityEngine;
 public class CookingPlate : MonoBehaviour
 {
     public bool InThePlace = false;
+    private bool iscooking = false;
+    private int cnt = 0;
+    private int cutnum = 0;
+    private GameObject Newfood;
+    public float FlyForce = 10f;
+
+
+    [Header("食物（pieces）引入")]
+    public GameObject Redmeat;
+
+    [Header("食物生成位置引入")]
+    public GameObject RedmeatPlace;
 
     // Start is called before the first frame update
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "RedMeat")
+        if (other.tag == "RedMeat" && iscooking == false)
         {
-            InThePlace = true;
-            print("Play the Animation");
-        }
-        
+            DealWithFood(4, Redmeat, RedmeatPlace);
+        }  
+    }
 
+    private void OnMouseDown()
+    {
+        if (iscooking == true)
+        {
+            CutFood(Newfood);
+        }
     }
 
     void Start()
@@ -27,5 +44,43 @@ public class CookingPlate : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void CutFood(GameObject foodpieces)
+    {
+        Transform pieces = foodpieces.transform.GetChild(cnt);
+ //       pieces.SetParent(null);
+
+        Rigidbody rb = pieces.GetComponent<Rigidbody>();
+        LetFoodJump(rb);
+
+        cnt++;
+        if(cnt == cutnum)
+        {
+            print("intopot");
+            cnt = 0;
+            iscooking = false;
+        }
+    }
+
+    void DealWithFood(int num, GameObject food, GameObject place)
+    {
+        InThePlace = true;
+        iscooking = true;
+
+        Newfood = Instantiate(food, place.transform.position, Quaternion.identity);
+        cutnum = num;
+    }
+
+    void LetFoodJump(Rigidbody piece)
+    {
+        piece.isKinematic = false;
+        Vector3 randomForce = new Vector3(
+            Random.Range(-2f, 2f),
+            Random.Range(5f, 8f),
+            Random.Range(-2f, 2f)
+        );
+        piece.AddForce(randomForce * FlyForce);
+        print("Flying");
     }
 }
